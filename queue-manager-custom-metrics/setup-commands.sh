4663e7730efe59f01eba7c2444e2de0d4cf3769b
Martin Evans <martin.evans1@ibm.com>
2025-05-30 10:08:23 +0100

    Add secrets link to service account

--- queue-manager-custom-metrics/setup-commands.sh
+++ queue-manager-custom-metrics/setup-commands.sh
@@ -2,10 +2,11 @@
 
 # create secrets for the image registries
 oc create secret docker-registry my-docker-registry-creds --docker-server=docker.io --docker-username=username --docker-password=password --docker-email=email
-oc secrets link pipeline my-docker-registry-creds
+oc secrets link pipeline my-docker-registry-creds --for pull
 
 # create credentials for the buildah task for pushing to a registry
 oc create secret docker-registry internal-registry-creds --docker-server=image-registry.openshift-image-registry.svc:5000 --docker-username=kubeadmin --docker-password=`oc whoami -t` --docker-email=email
+oc secrets link pipeline internal-registry-creds
 
 # run using a template
 oc create -f pipeline-run-template.yaml
